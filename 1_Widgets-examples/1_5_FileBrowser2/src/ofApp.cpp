99f387d0066c49c3e8e4014b0ead0b31577fa6b8
moebiussurfing <moebiussurfing@gmail.com>
2021-07-31 01:28:10 -0300

    fixing Mosaic fileBrowser

--- 1_Widgets-examples/1_5_FileBrowser2/src/ofApp.cpp
+++ 1_Widgets-examples/1_5_FileBrowser2/src/ofApp.cpp
@@ -32,12 +32,21 @@ void ofApp::draw() {
 	//std::string& label
 	//fileBrowser.setIsRetina(false);
 
-	ImVec2& sz_xy = ImVec2(600, 600);
+	ImVec2 sz_xy = ImVec2(600, 600);
 	std::string valid_types = "xml";
 	std::string nameProposal = "nameProposal";
 
 	fileBrowser.showFileDialog("label", imgui_addons::ImGuiFileBrowser::DialogMode::SELECT, sz_xy, valid_types);
-
+    
+// https://github.com/d3cod3/ofxVisualProgramming/blob/master/src/objects/data/FileToData.cpp
+//    // file dialog
+//    if(ImGuiEx::getFileDialog(fileDialog, openFileFlag, "Open a previously saved Mosaic data file", imgui_addons::ImGuiFileBrowser::DialogMode::OPEN, ".txt", "", scaleFactor)){
+//        ofLog(OF_LOG_NOTICE,"START IMPORTING DATA");
+//        ofFile file (fileDialog.selected_path);
+//        tmpFileName = file.getFileName();
+//        filepath = file.getAbsolutePath();
+//        loadDataFile(filepath);
+//    }
 
 	//if (ofxImGuiSurfing::drawGui_FileDialog()) 
 	//{
