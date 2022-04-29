c8bafa43726ab7b28fc539ae45b8b6da1bdcbe40
moebiusSurfing <moebiussurfing@gmail.com>
2022-04-28 22:11:21 -0300

    +

--- 1_Widgets/1_8_Knobs/src/ofApp.cpp
+++ 1_Widgets/1_8_Knobs/src/ofApp.cpp
@@ -41,15 +41,15 @@ void ofApp::draw() {
 				// Legacy Non Styled Knobs ofParams
 
 				ImGui::Text("LEGACY NON STYLED KNOBS OF PARAMS");
-				ofxImGuiSurfing::AddSpacingDouble(); 
-				
+				ofxImGuiSurfing::AddSpacingDouble();
+
 				guiManager.Add(valueKnob1, OFX_IM_KNOB, 4, true);
 				guiManager.Add(valueKnob2, OFX_IM_KNOB, 4, true);
 				guiManager.Add(valueKnob3, OFX_IM_KNOB, 4, true);
 				guiManager.Add(valueKnob4, OFX_IM_KNOB, 4, false);
 
 				ofxImGuiSurfing::AddKnob(valueKnob2);
-				
+
 				ofxImGuiSurfing::AddSpacingSeparated();
 
 				//-
@@ -63,6 +63,7 @@ void ofApp::draw() {
 				ofxImGuiSurfing::AddSpacingDouble();
 
 				float size1 = 60;
+
 				ofxImGuiSurfing::AddKnobStyled(valueKnob1, OFX_IMGUI_KNOB_TICKKNOB, size1, "%.1f");
 				ImGui::SameLine();
 				ofxImGuiSurfing::AddKnobStyled(valueKnob2, OFX_IMGUI_KNOB_DOTKNOB, size1, "%.1f dB");
@@ -77,13 +78,20 @@ void ofApp::draw() {
 				ofxImGuiSurfing::AddSpacingDouble();
 
 				float size2 = 90;
-				ofxImGuiSurfing::AddKnobStyled(valueKnob5, OFX_IMGUI_KNOB_TICKKNOB, size2);
-				ImGui::SameLine();
-				ofxImGuiSurfing::AddKnobStyled(valueKnob6, OFX_IMGUI_KNOB_DOTKNOB, size2);
 
-				ofxImGuiSurfing::AddKnobStyled(valueKnob7, OFX_IMGUI_KNOB_WIPERKNOB, size2);
+				ImGuiKnobFlags flags;
+				flags += ImGuiKnobFlags_ValueTooltip;
+				flags += ImGuiKnobFlags_NoTitle;
+				flags += ImGuiKnobFlags_NoInput;
+				flags += ImGuiKnobFlags_DragHorizontal;
+				ofxImGuiSurfing::AddKnobStyled(valueKnob5, OFX_IMGUI_KNOB_TICKKNOB, size2, "-1", flags);
+
 				ImGui::SameLine();
-				ofxImGuiSurfing::AddKnobStyled(valueKnob8, OFX_IMGUI_KNOB_WIPERONLYKNOB, size2);
+				ofxImGuiSurfing::AddKnobStyled(valueKnob6, OFX_IMGUI_KNOB_WIPERDOTKNOB, size2);
+
+				ofxImGuiSurfing::AddKnobStyled(valueKnob7, OFX_IMGUI_KNOB_STEPPEDKNOB, size2);
+				ImGui::SameLine();
+				ofxImGuiSurfing::AddKnobStyled(valueKnob8, OFX_IMGUI_KNOB_SPACEKNOB, size2);
 
 				//--
 
@@ -104,7 +112,7 @@ void ofApp::draw() {
 				}
 				ImGui::SameLine();
 
-				if (ImGuiKnobs::Knob("Value 2", &value2, -6.0f, 6.0f, 12 / 350.f, "%.1fdB", ImGuiKnobVariant_WiperDot, size, ImGuiKnobFlags_DragHorizontal))
+				if (ImGuiKnobs::Knob("Value 2", &value2, -6.0f, 6.0f, 12 / 350.f, "%.1fdB", ImGuiKnobVariant_WiperDot, size, flags))
 				{
 				}
 				ImGui::SameLine();
