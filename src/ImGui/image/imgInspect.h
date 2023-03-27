a07ab8abfe914b4fa1107151c7b5eac2406c7fdc
moebiusSurfing <moebiussurfing@gmail.com>
2023-03-26 22:48:48 -0300

    working but only for 32 bits images

--- src/ImGui/image/imgInspect.h
+++ src/ImGui/image/imgInspect.h
@@ -59,6 +59,7 @@
 
 namespace ImageInspect
 {
+	/*
 	inline void histogram(const int width, const int height, const unsigned char* const bits)
 	{
 		unsigned int count[4][256] = { 0 };
@@ -130,8 +131,10 @@ namespace ImageInspect
 			}
 		}
 	}
+	*/
 
-	inline void drawNormal(ImDrawList* draw_list, const ImRect& rc, float x, float y, ofColor* c = nullptr)
+	/*
+	inline void drawNormal(ImDrawList* draw_list, const ImRect& rc, float x, float y)
 	{
 		draw_list->AddCircle(rc.GetCenter(), rc.GetWidth() / 2.f, 0x20AAAAAA, 24, 1.f);
 		draw_list->AddCircle(rc.GetCenter(), rc.GetWidth() / 4.f, 0x20AAAAAA, 24, 1.f);
@@ -140,160 +143,208 @@ namespace ImageInspect
 		//fix
 		//draw_list->AddLine(rc.GetCenter(), rc.GetCenter() + ImVec2(x * (rc.GetWidth() / 2.f), y * (rc.GetWidth() / 2.f)) , 0xFF0000FF, 2.f);
 	}
+	*/
 
 	inline void inspect(const int width,
 		const int height,
 		const unsigned char* const bits,
 		ImVec2 mouseUVCoord_,
-		ImVec2 displayedTextureSize, ofColor* c = nullptr)
+		ImVec2 displayedTextureSize,
+		bool bdebug = false,
+		int* zoomSize_ = nullptr,
+		int* zoomRectangleWidth_ = nullptr,
+		ofColor* c = nullptr)
 	{
+		////TODO: expose
+		//static int zoomSize = 4;
+		//static const float zoomRectangleWidth = 160.f;
+
+		//default
+		int zoomSize = 4;
+		float zoomRectangleWidth = 160.f;
+		if (zoomSize_ != nullptr && zoomRectangleWidth_ != nullptr) {
+			zoomSize = *zoomSize_;
+			zoomRectangleWidth = *zoomRectangleWidth_;
+		}
+		
+		bool bdebug2 = 0;
+
+		//-
+
+		// clamp
 		ImClamp(mouseUVCoord_, ImVec2{ 0,0 }, ImVec2{ 1,1 });
 		ImVec2 mouseUVCoord = mouseUVCoord_;
 
-		//ImVec2 mouseUVCoord = ImVec2(
-		//	ofClamp(mouseUVCoord_.x, 0, 1),
-		//	ofClamp(mouseUVCoord_.y, 0, 1));
-
-		//ImVec2 mouseUVCoord = mouseUVCoord.ImClamp(
-		//	ofClamp(mouseUVCoord_.x, 0, 1),
-		//	ofClamp(mouseUVCoord_.y, 0, 1));
-
-		ImGui::BeginTooltip();
-
-		ImGui::BeginGroup();
-
-		ImDrawList* draw_list = ImGui::GetWindowDrawList();
-
-		static const float zoomRectangleWidth = 160.f;
-
-		// 1. Magnifier
-
-		// bitmap zoom
-		ImGui::InvisibleButton("AnotherInvisibleMan", ImVec2(zoomRectangleWidth, zoomRectangleWidth));
-
-		const ImRect pickRc(ImGui::GetItemRectMin(), ImGui::GetItemRectMax());
-		draw_list->AddRectFilled(pickRc.Min, pickRc.Max, 0xFF000000);
-		static int zoomSize = 4;
-		const float quadWidth = zoomRectangleWidth / float(zoomSize * 2 + 1);
-		const ImVec2 quadSize(quadWidth, quadWidth);
 		const int basex = ImClamp(int(mouseUVCoord.x * width), zoomSize, width - zoomSize);
 		const int basey = ImClamp(int(mouseUVCoord.y * height), zoomSize, height - zoomSize);
 
-		for (int y = -zoomSize; y <= zoomSize; y++)
+		//-
+
+		if (bdebug) {
+			ImVec2 size_min{ zoomRectangleWidth + 150, -1 };
+			ImVec2 size_max{ 1000,1000 };
+			//ImVec2 size_max{ -1,-1 };
+			ImGui::SetNextWindowSizeConstraints(size_min, size_max);
+		}
+
+		//-
+
+		ImGui::BeginTooltip();
 		{
-			for (int x = -zoomSize; x <= zoomSize; x++)
+			ImGui::BeginGroup();
 			{
-				//TODO; should skip when x or y < 0
-				
-				//fix
-				//uint32_t texel = ((uint32_t*)bits)[(basey - y) * width + x + basex];
-				size_t i = (basey - y) * width + x + basex;
+				ImDrawList* draw_list = ImGui::GetWindowDrawList();
 
-				//size_t size = sizeof(((uint32_t*)bits)) / sizeof(((uint32_t*)bits)[0]);
-				//size_t sz = ((uint32_t*)bits).size();
-				//size_t size = sizeof(((uint32_t*)bits)) / sizeof(((uint32_t*)bits)[0]);
+				// 1. Magnifier
 
-				//TODO:
-				size_t szt = sizeof((uint32_t*)bits);
-				i = MIN(i, szt - 1);
+				// bitmap zoom
+				ImGui::InvisibleButton("AnotherInvisibleMan", ImVec2(zoomRectangleWidth, zoomRectangleWidth));
+				const ImRect pickRc(ImGui::GetItemRectMin(), ImGui::GetItemRectMax());
+
+				ImU32 c = 0xFF000000;
+				//ImU32 c = ImColor(0, 255, 0, 255);//0xFF000000
+
+				draw_list->AddRectFilled(pickRc.Min, pickRc.Max, c);
+
+				const float quadWidth = zoomRectangleWidth / float(zoomSize * 2 + 1);
+				const ImVec2 quadSize(quadWidth, quadWidth);
+
+				for (int y = -zoomSize; y <= zoomSize; y++)
+				{
+					for (int x = -zoomSize; x <= zoomSize; x++)
+					{
+						//TODO; should skip when x or y < 0
+
+						//fix
+						//uint32_t texel = ((uint32_t*)bits)[(basey - y) * width + x + basex];
+
+						size_t i = (basey - y) * width + x + basex;
+
+						//clamp
+						size_t sz = width * height;
+						static bool b = 1;
+						if (b) i = MIN(i, sz - 1);
+	
+						uint32_t texel = ((uint32_t*)bits)[i];
+
+						ImVec2 pos = pickRc.Min + ImVec2(float(x + zoomSize), float(y + zoomSize)) * quadSize;
+						draw_list->AddRectFilled(pos, pos + quadSize, texel);
+					}
+				}
+
+				if (1)
+				{
+					ImGui::SameLine();
+
+					// center quad
+					const ImVec2 pos = pickRc.Min + ImVec2(float(zoomSize), float(zoomSize)) * quadSize;
+
+					ImU32 c = ImColor(0, 0, 0, 200);//black
+					//ImU32 c = ImColor(255, 0, 0, 255);//red
+
+					draw_list->AddRect(pos, pos + quadSize, c, 0.f, 15, 2.f);
+				}
+
+				/*
+				if (0)
+				{
+					// normal direction
+					ImGui::InvisibleButton("AndOneMore", ImVec2(zoomRectangleWidth, zoomRectangleWidth));
+					ImRect normRc(ImGui::GetItemRectMin(), ImGui::GetItemRectMax());
+					for (int y = -zoomSize; y <= zoomSize; y++)
+					{
+						for (int x = -zoomSize; x <= zoomSize; x++)
+						{
+							uint32_t texel = ((uint32_t*)bits)[(basey - y) * width + x + basex];
+							const ImVec2 posQuad = normRc.Min + ImVec2(float(x + zoomSize), float(y + zoomSize)) * quadSize;
+							//draw_list->AddRectFilled(pos, pos + quadSize, texel);
+							const float nx = float(texel & 0xFF) / 128.f - 1.f;
+							const float ny = float((texel & 0xFF00) >> 8) / 128.f - 1.f;
+							const ImRect rc(posQuad, posQuad + quadSize);
+							drawNormal(draw_list, rc, nx, ny);
+						}
+					}
+				}
+				*/
+
+			}
+			ImGui::EndGroup();
+
+			ImGui::SameLine();
+
+			ImGui::BeginGroup();
+			{
+				// 2. Labels
+
+				// fix
+				//uint32_t texel = ((uint32_t*)bits)[(basey - zoomSize * 2 - 1) * width + basex];
+
+				size_t i = (basey - zoomSize * 2 - 1) * width + basex;
+
+				//clamp
+				size_t sz = width * height;
+				static bool b = 1;
+				if (b) i = MIN(i, sz - 1);
+				if (0) {
+					ImGui::Text("sz: %d", sz);
+					ImGui::Text("i:  %d", i);
+					ImGui::Spacing();
+				}
 
 				uint32_t texel = ((uint32_t*)bits)[i];
 
-				ImVec2 pos = pickRc.Min + ImVec2(float(x + zoomSize), float(y + zoomSize)) * quadSize;
-				draw_list->AddRectFilled(pos, pos + quadSize, texel);
-			}
-		}
-
-		if (1)
-		{
-			ImGui::SameLine();
-
-			// center quad
-			const ImVec2 pos = pickRc.Min + ImVec2(float(zoomSize), float(zoomSize)) * quadSize;
-			draw_list->AddRect(pos, pos + quadSize, 0xFF0000FF, 0.f, 15, 2.f);
-		}
-
-		if (0)
-		{
-			// normal direction
-			ImGui::InvisibleButton("AndOneMore", ImVec2(zoomRectangleWidth, zoomRectangleWidth));
-			ImRect normRc(ImGui::GetItemRectMin(), ImGui::GetItemRectMax());
-			for (int y = -zoomSize; y <= zoomSize; y++)
-			{
-				for (int x = -zoomSize; x <= zoomSize; x++)
+				// Get to parent color ptr
+				ImVec4 color = ImColor(texel);
+				if (c != nullptr)
 				{
-					uint32_t texel = ((uint32_t*)bits)[(basey - y) * width + x + basex];
-					const ImVec2 posQuad = normRc.Min + ImVec2(float(x + zoomSize), float(y + zoomSize)) * quadSize;
-					//draw_list->AddRectFilled(pos, pos + quadSize, texel);
-					const float nx = float(texel & 0xFF) / 128.f - 1.f;
-					const float ny = float((texel & 0xFF00) >> 8) / 128.f - 1.f;
-					const ImRect rc(posQuad, posQuad + quadSize);
-					drawNormal(draw_list, rc, nx, ny);
+					c->set(color);
 				}
+
+				if (bdebug) {
+					if (bdebug2) {
+
+						// uv	
+						ImGui::Text("U,V:    %1.1f,%1.1f", mouseUVCoord.x, mouseUVCoord.y);
+
+						// size
+						ImGui::Text("w,h:    %d,%d", int(displayedTextureSize.x), int(displayedTextureSize.y));
+
+						// coord
+						ImGui::Text("x,y:    %d,%d", int(mouseUVCoord.x * width), int(mouseUVCoord.y * height));
+
+						ImGui::Separator();
+					}
+
+					// rgb
+					ImGui::Text("R,G,B:  %d,%d,%d", int(color.x * 255.f), int(color.y * 255.f), int(color.z * 255.f));
+
+					// alpha
+					ImGui::Text("alpha:  %1.1f", color.w);
+
+					// color
+					/*
+					if (0) {
+						ImVec4 colHSV;
+						ImGui::ColorConvertRGBtoHSV(color.x, color.y, color.z, colHSV.x, colHSV.y, colHSV.z);
+						//ImGui::Separator();
+						//ImGui::Text("R 0x%02x  G 0x%02x  B 0x%02x", int(color.x * 255.f), int(color.y * 255.f), int(color.z * 255.f));
+						//ImGui::Text("R %1.3f G %1.3f B %1.3f", color.x, color.y, color.z);
+						//ImGui::Separator();
+						//ImGui::Text(
+						//"H 0x%02x  S 0x%02x  V 0x%02x", int(colHSV.x * 255.f), int(colHSV.y * 255.f), int(colHSV.z * 255.f));
+						ImGui::Text("H %1.3f S %1.3f V %1.3f", colHSV.x, colHSV.y, colHSV.z);
+						//ImGui::Separator();
+						//ImGui::Text("Alpha 0x%02x", int(color.w * 255.f));
+						ImGui::Text("alpha %1.3f", color.w);
+					}
+					*/
+				}
+				//ImGui::Separator();
 			}
+			ImGui::EndGroup();
+
+			//histogram(width, height, bits);
 		}
-
-		ImGui::EndGroup();
-
-		ImGui::SameLine();
-
-		// 2. Labels
-
-		ImGui::BeginGroup();
-
-		//uint32_t texel = ((uint32_t*)bits)[(basey - zoomSize * 2 - 1) * width + basex];
-		//fix
-		float z = zoomSize * 2;
-		size_t i = (basey - z - 1) * width + basex;
-		//size_t sz = ((uint32_t*)bits).size();
-		//size_t sz = IM_ARRAYSIZE(bits);
-		size_t sz = sizeof(bits);
-		size_t szt = sizeof((uint32_t*)bits);
-
-		i = MIN(i, szt - 1);
-		uint32_t texel = ((uint32_t*)bits)[i];
-
-		// size
-		ImGui::Text("w,h %d,%d", int(displayedTextureSize.x), int(displayedTextureSize.y));
-
-		// uv	
-		ImGui::Text("U,V %1.2f,%1.2f", mouseUVCoord.x, mouseUVCoord.y);
-
-		// coord
-		ImGui::Text("x,y %d,%d", int(mouseUVCoord.x * width), int(mouseUVCoord.y * height));
-
-		// parent color ptr
-		ImVec4 color = ImColor(texel);
-		if (c != nullptr)
-		{
-			c->set(color);
-		}
-
-		// color
-		if (0) {
-			ImVec4 colHSV;
-			ImGui::ColorConvertRGBtoHSV(color.x, color.y, color.z, colHSV.x, colHSV.y, colHSV.z);
-			//ImGui::Separator();
-			//ImGui::Text("R 0x%02x  G 0x%02x  B 0x%02x", int(color.x * 255.f), int(color.y * 255.f), int(color.z * 255.f));
-			ImGui::Text("R %1.3f G %1.3f B %1.3f", color.x, color.y, color.z);
-
-			//ImGui::Separator();
-
-			//ImGui::Text(
-				//"H 0x%02x  S 0x%02x  V 0x%02x", int(colHSV.x * 255.f), int(colHSV.y * 255.f), int(colHSV.z * 255.f));
-			ImGui::Text("H %1.3f S %1.3f V %1.3f", colHSV.x, colHSV.y, colHSV.z);
-
-			//ImGui::Separator();
-
-			//ImGui::Text("Alpha 0x%02x", int(color.w * 255.f));
-			ImGui::Text("Alpha %1.3f", color.w);
-		}
-
-		//ImGui::Separator();
-
-		ImGui::EndGroup();
-
-		//histogram(width, height, bits);
 		ImGui::EndTooltip();
 	}
 } // namespace ImageInspect
