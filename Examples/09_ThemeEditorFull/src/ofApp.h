234bc6c0965efd5a72978d24677999c2faa752e7
moebiusSurfing <moebiussurfing@gmail.com>
2023-06-10 05:37:40 -0300

    Update ofApp.h

--- Examples/09_ThemeEditorFull/src/ofApp.h
+++ Examples/09_ThemeEditorFull/src/ofApp.h
@@ -1,8 +1,24 @@
 #pragma once
 
 /*
+ 
+    This example shows how to explore many fonts and styles on runtime, 
+    to fine tune your app ui.
+    Default font file and size will be serialized into data folder:
+    \Examples\09_ThemeEditorFull\bin\data\Gui\DefaultFont.json
+
+    BTW, is not necessary to instantiate SurfingThemeEditor to change
+    the default font. It's just a kind of tool for exploring possibilities,
+    trying different font styles.
+    
+    As you can see on the 09_ThemeEditor, 
+    you can hard-code a default font replacement doing:
+
+    ui.setDefaultFont(ofToDataPath("assets/fonts/telegrama_render.otf"), 14);
 
 */
+
+//--
 
 #include "ofMain.h"
 
@@ -22,5 +38,4 @@ public:
 
     SurfingThemeEditor e;
     SurfingFonts f;
-
 };
